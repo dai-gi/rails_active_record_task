62fd1bdde0ea5f1616a7f5258f29c31b11e121b6
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-07-18 00:32:15 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def orders_count
+    self.count
+  end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def orders_count
+    self.count
+  end
 end
