a3aaf157794b26ac78143e0268c1cd5f14c2d1f7
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-07-19 00:56:39 +0900

    Improve: 修正

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -8,14 +8,14 @@ class ExercisesController < ApplicationController
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
-    @shops = Shop.left_outer_joins(foods: :order_foods).distinct.where(order_foods: { id: nil })
+    @shops = Shop.left_outer_joins(foods: :order_foods).where(order_foods: { id: nil })
   end
 
   def exercise3
     # 【要件】配達先の一番多い住所を返すこと
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-    @address = Address.joins(:orders).where(postalcode: Address.joins(:orders).group(:postalcode).order('count_all DESC').count.first.first).pluck(:prefecture, :city, :line_1, :line_2).first.join
+    @address = Address.joins(:orders).where(postalcode: Address.joins(:orders).group(:postalcode).order('count_all DESC').count.first.first).pluck(:postalcode).first
   end
 
 
